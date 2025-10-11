f6e39683b388101b960c403df453bae012b76662
sceptzrion <ikhsan.ry@gmail.com>
2025-10-12 01:48:38 +0700

    Add responsive for login page

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,8 +2,8 @@ import Footer from "@/components/footer/footerauth";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-[#00acc1]">
-      <div className="flex flex-col w-[416px] h-full mt-18 mb-24 rounded-sm p-9 bg-white">
+    <main className="min-h-screen p-3 sm:p-0 flex flex-col items-center justify-center bg-[#00acc1]">
+      <div className="flex flex-col w-full sm:w-[516px] md:w-[456px] xl:w-[416px] h-full mt-18 mb-24 rounded-sm p-9 bg-white">
         {children}
       </div>
       <Footer />
--- components/footer/footerauth.tsx
+++ components/footer/footerauth.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex flex-row items-center justify-center p-5 text-[#FFFFFF80] mt-auto text-sm gap-1">
-        <p>2025 © 'SISKA' prototype by </p>
-        <a href="#" className=''>PERSONA</a>
-        <p>for educational purposes</p>
+        <p className='text-center'>2025 © 'SISKA' prototype by
+        <a href="#"> PERSONA </a>
+        for educational purposes</p>
     </div>
   )
 }
