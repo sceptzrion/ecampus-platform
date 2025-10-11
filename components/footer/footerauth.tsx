1ced152b20f59f9f56ee60cc7a26be78a7c8d009
sceptzrion <ikhsan.ry@gmail.com>
2025-10-12 01:21:07 +0700

    fix: Add mt-auto class to footer for consistent layout alignment

--- components/footer/footerauth.tsx
+++ components/footer/footerauth.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex flex-row items-center justify-center p-5 text-[#FFFFFF80] text-sm gap-1">
+    <div className="flex flex-row items-center justify-center p-5 text-[#FFFFFF80] mt-auto text-sm gap-1">
         <p>2025 © 'SISKA' prototype by </p>
         <a href="#" className=''>PERSONA</a>
         <p>for educational purposes</p>
--- components/footer/footermain.tsx
+++ components/footer/footermain.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="flex flex-row items-center justify-center p-5 text-[#9BA9B0] text-sm gap-1">
+    <div className="flex flex-row items-center justify-center p-5 text-[#9BA9B0] text-sm mt-auto gap-1">
         <p>2025 © 'SISKA' prototype by </p>
         <a href="#" className='text-[#6658DD]'>PERSONA</a>
         <p>for educational purposes</p>
